0c8231feec3b994b6e08e1d7a4642b676edb10c4
PC-20171116IVEC\Administrator <331258747@qq.com>
2019-10-31 18:27:42 +0800

    1

--- app/src/main/java/cn/lex_mung/client_android/mvp/ui/adapter/MyOrderAdapter2.java
+++ app/src/main/java/cn/lex_mung/client_android/mvp/ui/adapter/MyOrderAdapter2.java
@@ -29,6 +29,7 @@ public class MyOrderAdapter2 extends BaseQuickAdapter<OrderEntity, BaseViewHolde
             helper.setText(R.id.item_tv_title, item.getOrderType());
         }
 
+        //价格
         if(item.getBuyerPayAmount() > 0){
             helper.setGone(R.id.ll_price, true);
             helper.setText(R.id.tv_price, item.getBuyerPayAmountStr());
@@ -36,21 +37,33 @@ public class MyOrderAdapter2 extends BaseQuickAdapter<OrderEntity, BaseViewHolde
             helper.setGone(R.id.ll_price, false);
         }
 
+        //显示 享文书担保服务 法律顾问
         helper.setGone(R.id.ll_lawSuiId,false);
         if(!TextUtils.isEmpty(item.getLawSuiId())){
             helper.setGone(R.id.ll_lawSuiId,true);
         }
 
+        //状态
         helper.setText(R.id.item_tv_status, item.getStatusValue());
-        if(item.getOrderStatus() == 3 || item.getOrderStatus() == 7){//3已关闭，7已完成
-            helper.setBackgroundRes(R.id.item_tv_status,R.drawable.round_100_f8f8f8_all_d8d8d8);
-            helper.setTextColor(R.id.item_tv_status,ContextCompat.getColor(mContext,R.color.c_d8d8d8));
+        if(item.getTypeId() == 7){//法律顾问
+            if(item.getOrderStatus() == 8 || item.getOrderStatus() == 9){//订单状态：1待接单（空） 2待服务（1） 3服务中（1） 4待确认（1） 5待评价（3） 6已完成（3） 7待处理（3） 8已关闭（3） 9已取消（0）
+                helper.setBackgroundRes(R.id.item_tv_status,R.drawable.round_100_f8f8f8_all_d8d8d8);
+                helper.setTextColor(R.id.item_tv_status,ContextCompat.getColor(mContext,R.color.c_d8d8d8));
+            }else{
+                helper.setBackgroundRes(R.id.item_tv_status,R.drawable.round_100_fae5db_all_eb5514);
+                helper.setTextColor(R.id.item_tv_status,ContextCompat.getColor(mContext,R.color.c_EB5514));
+            }
         }else{
-            helper.setBackgroundRes(R.id.item_tv_status,R.drawable.round_100_fae5db_all_eb5514);
-            helper.setTextColor(R.id.item_tv_status,ContextCompat.getColor(mContext,R.color.c_EB5514));
+            if(item.getOrderStatus() == 3 || item.getOrderStatus() == 7){//3已关闭，7已完成
+                helper.setBackgroundRes(R.id.item_tv_status,R.drawable.round_100_f8f8f8_all_d8d8d8);
+                helper.setTextColor(R.id.item_tv_status,ContextCompat.getColor(mContext,R.color.c_d8d8d8));
+            }else{
+                helper.setBackgroundRes(R.id.item_tv_status,R.drawable.round_100_fae5db_all_eb5514);
+                helper.setTextColor(R.id.item_tv_status,ContextCompat.getColor(mContext,R.color.c_EB5514));
+            }
         }
 
-
+        //内容
         if(!TextUtils.isEmpty(item.getContent()) || item.getTypeId() == 4){
             if(item.getTypeId() == 4){
                 helper.setText(R.id.item_tv_content, "咨询分类："+item.getTypeName());
